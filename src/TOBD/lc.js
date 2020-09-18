3bd1919e7f2583c04f09338614854a95b9ca13a8
dut-a <d.dut@live.com>
2020-09-17 21:12:41 -0500

    Individual service items

---
+++ src/TOBD/lc.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+
+function ListItem({ item }) {
+  return (
+    <Fragment>
+      <dt>{item.term}</dt>
+      <dd>{item.description}</dd>
+    </Fragment>
+  );
+}
+
+function ServiceItems(props) {
+  return (
+    <dl>
+      {props.items.map(item => (
+        <ListItem item={item} key={item.id} />
+      ))}
+    </dl>
+  );
+}
+
